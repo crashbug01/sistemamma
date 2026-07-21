aecb6d8fb1562ef1f92638427987427127a7eed8
Prima <mprima2003@gmail.com>
2026-07-21 09:46:24 +0700

    Initial commit

---
+++ database/migrations/2026_07_21_022249_peserta.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peserta', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['laki-laki', 'perempuan']);
+            $table->string('no_whatsapp');
+            $table->foreignId('surat_id');
+            $table->string('jam_belajar');
+            $table->enum('status', ['aktif', 'tidak_aktif']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peserta');
+    }
+};
